08506452ab1a50754a83e3d865592279cca30bc5
viliuskacerginas <93251851+viliuskacerginas@users.noreply.github.com>
2022-01-08 23:14:56 +0200

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -148,3 +148,11 @@ gridN.addEventListener('click', () => {
   }
 })
 // BURGER                        BURGER                             BURGER
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
